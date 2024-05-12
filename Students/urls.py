cd1696392b247ee90b7e9936415e9002fd5cc6dd
Ant1326 <edwarquianzon@addu.edu.ph>
2024-05-13 03:24:23 +0800

    Update urls.py add deleteStudents and updateStudents

--- Students/urls.py
+++ Students/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.studentList, name='studentList'),
     path('add/', views.addStudent, name='addStudent'),
+    path('delete/<int:student_id>/', views.deleteStudent, name='deleteStudent'),
+    path('edit/<int:student_id>/', views.updateStudent, name='updateStudent'),
 ]
